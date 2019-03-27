d210f7cbce59eaf6821a923f1d6fc1f9ad7d857d
Lars <lars.scheme@gmail.com>
2019-03-27 20:58:07 +0100

    Add formatting for title and message
    See the new comment at the top of main.go for a detailed explanation.

--- main.go
+++ main.go
@@ -1,10 +1,43 @@
+/*
+This is a Windows editor to use for git commit messages. To use it as the
+default editor, put the executable somewhere in your PATH and tell git about it:
+
+	git config --global core.editor "commited"
+
+Here are seven rules for git commit messages from
+
+	https://chris.beams.io/posts/git-commit
+
+1. Separate subject from body with a blank line
+2. Limit the subject line to 50 characters
+3. Capitalize the subject line
+4. Do not end the subject line with a period
+5. Use the imperative mood in the subject line
+6. Wrap the body at 72 characters
+7. Use the body to explain what and why vs. how
+
+This editor helps following these rules:
+
+- title and message are separate input fields, the commit message will have a
+  blank line separating them (rule 1)
+- the title input field shows the title's length and displays a "!" for titles
+  longer then 50 characters (rule 2)
+- Ctrl+F formats the title to start with a capital letter and removes trailing
+  periods (rules 3 and 4)
+- Ctrl+F wraps the message text to a maximum of 72 characters per line (rule 6)
+
+Rules 5 and 7 are about the message's content so this is left to the author :-)
+*/
+
 package main
 
 import (
 	"io/ioutil"
 	"os"
 	"strconv"
+	"strings"
 	"time"
+	"unicode"
 	"unicode/utf8"
 
 	"github.com/gonutz/w32"
@@ -20,35 +53,50 @@ func main() {
 	msgPath := os.Args[1]
 
 	font, _ := wui.NewFont(wui.FontDesc{Name: "Tahoma", Height: -13})
+	fixedFont, _ := wui.NewFont(wui.FontDesc{Name: "Consolas", Height: -15})
 	w := wui.NewDialogWindow()
 	w.SetFont(font)
 	w.SetTitle("Enter Commit Message")
 	w.SetClientSize(800, 600)
 
-	ok := wui.NewLabel()
-	w.Add(ok)
-	ok.SetText("Press CTRL+ENTER to commit")
-	ok.SetBounds(0, 0, w.ClientWidth(), 20)
-	ok.SetCenterAlign()
+	writeLine := func(text string, y int) {
+		l := wui.NewLabel()
+		w.Add(l)
+		l.SetText(text)
+		l.SetBounds(0, y, w.ClientWidth(), 20)
+		l.SetCenterAlign()
+	}
+	writeLine("Press CTRL+ENTER to commit", 20)
+	writeLine("Press ESC to abort commit", 40)
+	writeLine("Press CTRL+F to format the title and message", 60)
 
-	esc := wui.NewLabel()
-	w.Add(esc)
-	esc.SetText("Press ESC to abort commit")
-	esc.SetBounds(0, 20, w.ClientWidth(), 20)
-	esc.SetCenterAlign()
+	titleCap := wui.NewLabel()
+	w.Add(titleCap)
+	titleCap.SetBounds(0, 100, 50, 25)
+	titleCap.SetRightAlign()
+	titleCap.SetText("Title")
 
 	title := wui.NewEditLine()
 	w.Add(title)
-	title.SetBounds(10, 40, w.ClientWidth()-70, 25)
+	title.SetBounds(60, 100, w.ClientWidth()-120, 25)
+	title.SetFont(fixedFont)
 
 	titleLength := wui.NewLabel()
 	w.Add(titleLength)
-	titleLength.SetBounds(w.ClientWidth()-50, 40, 40, 25)
+	titleLength.SetBounds(w.ClientWidth()-50, 100, 40, 25)
+
 	go func() {
+		// update the title length label when the title changes
+		// right now the wui library does not provide an event when typing in a
+		// text box, thus we just poll for changes 10 times a second
 		for {
-			n := strconv.Itoa(utf8.RuneCountInString(title.Text()))
-			if titleLength.Text() != n {
-				titleLength.SetText(n)
+			n := utf8.RuneCountInString(title.Text())
+			text := strconv.Itoa(n)
+			if n > 50 {
+				text += " !"
+			}
+			if titleLength.Text() != text {
+				titleLength.SetText(text)
 			}
 			time.Sleep(100 * time.Millisecond)
 		}
@@ -56,8 +104,37 @@ func main() {
 
 	text := wui.NewTextEdit()
 	w.Add(text)
-	text.SetBounds(10, 70, w.ClientWidth()-20, w.ClientHeight()-80)
+	text.SetBounds(10, 130, w.ClientWidth()-20, w.ClientHeight()-140)
+	text.SetFont(fixedFont)
 
+	w.SetShortcut(wui.ShortcutKeys{Mod: wui.ModControl, Rune: 'F'}, func() {
+		newTitle := title.Text()
+		newTitle = strings.TrimSpace(newTitle)
+		for {
+			// trim all spaces and periods, this might take several iterations,
+			// e.g. if the title is "blah . . ."
+			before := newTitle
+			newTitle = strings.TrimSpace(newTitle)
+			newTitle = strings.TrimSuffix(newTitle, ".")
+			if newTitle == before {
+				break
+			}
+		}
+		newTitle = capitalize(newTitle)
+		title.SetText(newTitle)
+
+		var newText string
+		lines := strings.Split(strings.TrimSpace(text.Text()), "\r\n")
+		for _, line := range lines {
+			a, b := splitLine(line)
+			newText += a + "\r\n"
+			for b != "" {
+				a, b = splitLine(b)
+				newText += a + "\r\n"
+			}
+		}
+		text.SetText(strings.TrimSuffix(newText, "\r\n"))
+	})
 	w.SetShortcut(wui.ShortcutKeys{Key: w32.VK_ESCAPE}, w.Close)
 	w.SetShortcut(wui.ShortcutKeys{Mod: wui.ModControl, Key: w32.VK_RETURN}, func() {
 		output := title.Text()
@@ -78,8 +155,33 @@ func main() {
 	}
 	w.SetShortcut(wui.ShortcutKeys{Key: w32.VK_TAB}, toggleFocus)
 	w.SetShortcut(wui.ShortcutKeys{Mod: wui.ModShift, Key: w32.VK_TAB}, toggleFocus)
+
 	w.SetOnShow(func() {
 		title.Focus()
 	})
 	w.Show()
+}
+
+// split line returns the two parts of a line if it is too long, or the line and
+// the empty string if the line is already short enough.
+func splitLine(s string) (a, b string) {
+	const maxLineLen = 72
+
+	if len(s) <= maxLineLen {
+		return s, ""
+	}
+
+	i := strings.LastIndex(s[:maxLineLen], " ")
+	if i == -1 {
+		return s, "" // do not split, leave it a long line
+	}
+	return s[:i], s[i+1:]
+}
+
+func capitalize(s string) string {
+	if s == "" {
+		return ""
+	}
+	r, size := utf8.DecodeRuneInString(s)
+	return string(unicode.ToUpper(r)) + s[size:]
 }
